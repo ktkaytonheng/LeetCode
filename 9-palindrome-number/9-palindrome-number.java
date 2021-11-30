6a6e14eec4d42d8787d822d67694d579fba3f7a4
Kayton <70306286+ktkaytonheng@users.noreply.github.com>
2021-12-01 01:09:58 +0800

    Time: 6 ms (99.97%), Space: 38.6 MB (59.59%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        // If number is negative, the '-' makes x a non-palindrome
         if (x < 0) return false;
+        // Single digits are considered palindromes
         if (x < 10) return true;
         
+        // Approach: To reverse the number and check if its equal the original
+        // Example 1: 123 would give 321, which gives false
+        // Example 2: 12521 would give 12521, which gives true
         int y = x;
         int temp = 0;
         
@@ -13,7 +18,5 @@ class Solution {
         }
         if (temp == y) return true;
         return false;
-        
-        
     }
 }
