db233ebddd5997d752b9c948b3d57794d16ed668
Kayton <70306286+ktkaytonheng@users.noreply.github.com>
2021-12-02 01:00:59 +0800

    Time: 0 ms (100.00%), Space: 39.3 MB (52.94%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {    
+        // Base condition have to account for if arr is already decreasing to begin with,
+        // or if last number is the largest number.
         int len = arr.length;
         if (len == 1 || arr[0] > arr[1]) return 0;
         if (arr[len-1] > arr[len-2]) return len-1;
-        
+        // Start pointer and end pointer, traversing towards the middle of the arr and returning
+        // if peak is found. For start pointer, condition is to check if next is smaller, and
+        // for end pointer, condition is to check if prev is smaller
         return helper(arr, 1, len-2);
     }
     
