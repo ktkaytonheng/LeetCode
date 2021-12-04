6980a3b81cf92b8d27cd7ed894053f230a19f924
Kayton <70306286+ktkaytonheng@users.noreply.github.com>
2021-12-05 00:42:00 +0800

    Time: 1 ms (100.00%), Space: 49.3 MB (26.69%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,5 +1,10 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        // Quite a difficult question honestly. Had to lookup solution.
+        // Basically, we want to instantiate a left sum that represents the sum of the left
+        // elements of the pivot, which will be iterating through the array. The condition
+        // to check for is sum - pivot - leftsum = leftsum, since
+        // rightSum = sum - pivot - leftsum.
         if (nums.length == 1) return 0;
         
         int sum = 0;
