b0e0df878d32871334dda9bc76ab5aadd7aeab79
Kayton <70306286+ktkaytonheng@users.noreply.github.com>
2021-12-05 00:03:52 +0800

    Time: 3 ms (37.45%), Space: 38.5 MB (33.38%) - LeetHub

--- 728-self-dividing-numbers/728-self-dividing-numbers.java
+++ 728-self-dividing-numbers/728-self-dividing-numbers.java
@@ -8,6 +8,10 @@ class Solution {
     }
     
     public boolean checkSelfDividing(int val) {
+        // First condition checks for single digit, which will always return true
+        if (val < 10) return true;
+        // While loop checks for every single digit, return false if 0 is found
+        // Return false also when val % digit is not 0 (not divisible)
         int temp = val;
         while (temp != 0) {
             int digit = temp % 10;
